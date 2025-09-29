2b1e2000a7e75bc1e6da781cc1f36005595fbb5a
Shaista Tariq <shaistatariq448@gmail.com>
2025-09-29 20:47:08 +0500

    All tasks added in one repo with folders

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import FunctionalComponent from "./assets/funcation";
+function App() {
+  return (
+    <div>
+      <FunctionalComponent name="Muhammad" secondName="Adnan" />
+      <FunctionalComponent name="Ali" secondName="Raza" />
+      <FunctionalComponent name="Sara" secondName="Khan" />
+    </div>
+  );
+}
+export default App;
---
+++ vite-project/src/assets/funcation.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function FunctionalComponet( {name, secondName} ) {
+  return (
+    <div>
+        <h1>FunctionalComponet 1</h1>
+        <h2>My name is {name} and my second name is {secondName}</h2>
+
+        </div>
+  )
+}
